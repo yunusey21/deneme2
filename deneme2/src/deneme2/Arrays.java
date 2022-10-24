00c6463956f2a628613fe188d933a7fd399054dc
emre <emre@10.121.3.144>
2022-10-24 10:11:56 +0300

    deneme2

---
+++ deneme2/src/deneme2/Arrays.java
@@ -0,0 +1,10 @@
+package deneme2;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ deneme2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author emre
+ *
+ */
+module deneme2 {
+}
